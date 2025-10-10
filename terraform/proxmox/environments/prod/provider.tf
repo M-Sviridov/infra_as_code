ce51a54f64d647bc7ab31168f26b26035dfd9290
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2025-10-10 11:40:12 +0700

    Add root@pam proxmox provider for privileged escalation (bind mounts)

--- terraform/proxmox/environments/prod/provider.tf
+++ terraform/proxmox/environments/prod/provider.tf
@@ -7,8 +7,18 @@ terraform {
   }
 }
 
+# Default provider - uses API token (less privileged)
 provider "proxmox" {
   endpoint  = var.virtual_environment_endpoint
   api_token = var.virtual_environment_api_token
   insecure  = true
+}
+
+# Privileged provider - uses root@pam credentials
+provider "proxmox" {
+  alias    = "root"
+  endpoint = var.virtual_environment_endpoint
+  username = var.virtual_environment_root_username
+  password = var.virtual_environment_root_password
+  insecure = true
 }
