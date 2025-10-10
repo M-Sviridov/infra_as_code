3e9d03f57e99af69fcaab3423c83bca77568948b
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2025-10-10 11:40:38 +0700

    Add root@pam proxmox variables

--- terraform/proxmox/environments/prod/variables.tf
+++ terraform/proxmox/environments/prod/variables.tf
@@ -9,6 +9,18 @@ variable "virtual_environment_api_token" {
   description = "Proxmox API Token (user@realm!provider=UUID)"
 }
 
+variable "virtual_environment_root_password" {
+  type        = string
+  sensitive   = true
+  description = "Proxmox root user password"
+}
+
+variable "virtual_environment_root_username" {
+  type        = string
+  default     = "root@pam"
+  description = "Proxmox root user"
+}
+
 variable "ssh_public_keys" {
   type        = list(string)
   description = "List of SSH public keys for root user"
