5a31f76efabc1015d4e641874314e6520d56498e
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2025-10-10 11:31:57 +0700

    Add outputs for prod-frigate host deployment

--- terraform/proxmox/environments/prod/outputs.tf
+++ terraform/proxmox/environments/prod/outputs.tf
@@ -8,3 +8,14 @@ output "fileserver_summary" {
     tags     = module.prod_fileserver.tags
   }
 }
+
+output "frigate_summary" {
+  description = "Complete summary"
+  value = {
+    vm_id    = module.prod_frigate.vm_id
+    hostname = module.prod_frigate.hostname
+    vlan     = module.prod_frigate.network_vlan_id
+    node     = module.prod_frigate.node_name
+    tags     = module.prod_frigate.tags
+  }
+}
