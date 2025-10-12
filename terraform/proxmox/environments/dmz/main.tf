64a92e934e2a6ee167a2d0951dd228de030ed58c
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2025-10-12 12:55:56 +0700

    Create dmz-auth server for Authentik

--- terraform/proxmox/environments/dmz/main.tf
+++ terraform/proxmox/environments/dmz/main.tf
@@ -35,3 +35,41 @@ module "dmz_freshrss" {
   network_mac_address = "BC:24:11:EA:A0:38"
   tags                = ["docker", "dmz", "opentofu"]
 }
+
+module "dmz_auth" {
+  source    = "../../modules/lxc"
+  providers = { proxmox = proxmox.root }
+
+  # Required variables
+  hostname         = "dmz-auth"
+  network_vlan_id  = 10
+  node_name        = "heimdall"
+  template_file_id = "local:vztmpl/debian-13-standard_13.1-2_amd64.tar.zst"
+  vm_id            = 100
+
+  # Secrets from variables
+  root_password   = var.root_password
+  ssh_public_keys = var.ssh_public_keys
+
+  # Optional variables
+  cpu_cores        = 2
+  description      = "Production DMZ Authentication Server through Authentik"
+  disk_size        = 10
+  memory_dedicated = 2048
+
+  mounts = [
+    {
+      acl    = true
+      path   = "/mnt/docker/appdata"
+      volume = "/flash/docker/appdata/dmz-auth"
+    },
+    {
+      acl    = true
+      path   = "/mnt/docker/stacks"
+      volume = "/flash/docker/stacks/dmz-auth"
+    },
+  ]
+
+  network_mac_address = "BC:24:11:F8:AD:59"
+  tags                = ["docker", "dmz", "opentofu"]
+}
--- terraform/proxmox/environments/dmz/outputs.tf
+++ terraform/proxmox/environments/dmz/outputs.tf
@@ -8,3 +8,14 @@ output "freshrss_summary" {
     tags     = module.dmz_freshrss.tags
   }
 }
+
+output "auth_summary" {
+  description = "Complete summary"
+  value = {
+    vm_id    = module.dmz_auth.vm_id
+    hostname = module.dmz_auth.hostname
+    vlan     = module.dmz_auth.network_vlan_id
+    node     = module.dmz_auth.node_name
+    tags     = module.dmz_auth.tags
+  }
+}
